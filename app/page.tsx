d2e40b49a622b35a48dc0db438152235e937836e
raulmahya <raulmahya11@gmail.com>
2023-10-04 19:37:43 +0700

    revision color and text

--- app/page.tsx
+++ app/page.tsx
@@ -20,8 +20,8 @@ export default async function Home({ searchParams }: HomeProps) {
 
       <div className='mt-12 padding-x padding-y max-width' id='discover'>
         <div className='home__text-container'>
-          <h1 className='text-4xl font-extrabold'>Car Catalogue</h1>
-          <p>Explore out cars you might like</p>
+          <h1 className='text-4xl font-extrabold'>T-Shirt Catalogue</h1>
+          <p>Explore out T-Shirt you might like</p>
         </div>
 
         <div className='home__filters'>
