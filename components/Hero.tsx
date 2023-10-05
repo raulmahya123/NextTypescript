2bc4268d5704b1f1b24e19e0e0bb402ade3b6545
raulmahya <raulmahya11@gmail.com>
2023-10-05 15:28:02 +0700

    fixing push

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          <Image src="/fix.png" alt="hero" fill className="object-contain" />
+          <Image src="/321.png" alt="hero" fill className="object-contain" />
         </div>
 
         <div className="hero__image-overlay" />
