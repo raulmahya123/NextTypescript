6fa1566e05785e34ab1c8f7fb54c1fbebf74199f
raulmahya <raulmahya11@gmail.com>
2023-10-09 13:40:17 +0700

    ADSASASD

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,14 +17,14 @@ const Hero = () => {
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
       <h1 className="hero__title">
-           Wellcome To <span className="text-green-900">Toko Informatika</span>
+           Wellcome To <span className="text-green-900">CODE REBEL</span>
         </h1>
         <p className="hero_subtitle">
-        Toko Informatika is the ultimate destination for high-quality UMKM (Micro, Small, and Medium Enterprises) fashion from Bandung,
+        CODE REBEL is the ultimate destination for high-quality UMKM (Micro, Small, and Medium Enterprises) fashion from Bandung,
         <br></br>
         featuring the latest IT-inspired designs. Here, you can discover top-notch products that reflect the creativity of local entrepreneurs. 
         <br></br>
-        'Toko Informatika not only offers  <span className="text-green-900">innovative styles but also supports the economic growth of Bandung. </span>
+        'CODE REBEL not only offers  <span className="text-green-900">innovative styles but also supports the economic growth of Bandung. </span>
         <br></br>
         Explore a range of unique clothing options with the latest designs and be part of the movement to support Bandung's UMKM sector.
         </p>
